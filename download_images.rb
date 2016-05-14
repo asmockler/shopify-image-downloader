aee18d29ee0f94a027d99935e0c784f4035036c8
Andy Mockler <asmockler@users.noreply.github.com>
2016-05-14 16:48:29 -0400

    :open_file_folder:

--- download_images.rb
+++ download_images.rb
@@ -5,9 +5,15 @@ products_file = File.read('products.json')
 products_file_json = JSON.parse(products_file)
 products = products_file_json["products"]
 
+system 'mkdir', '-p', 'images'
+
 products.each do |product|
+  path = "images/#{ product["title"] }"
+  system 'mkdir', '-p', path
   product["images"].each do |image|
     url = image["src"].split("?v")[0]
-    File.open(url.split("/")[-1], 'wb') { |file| file.write open(url).read }
+    filename = url.split('/')[-1]
+    File.open("#{path}/#{filename}", 'wb') { |file| file.write open(url).read }
   end
 end
+
